91c1e0d79a6aff6e5c584c093f086db4c3a93226
Timothy Hutton <timothyhutton@timothys-mbp-2.home>
2020-08-11 18:32:57 +0100

    paypal button

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-const CartTotals = ({value}) => {
+const CartTotals = ({value,history}) => {
     const{cartSubTotal,cartTax,cartTotal,clearCart} = value;
     return (
         <React.Fragment>
@@ -31,6 +32,7 @@ const CartTotals = ({value}) => {
                             </span>
                             <strong>$ {cartTotal}</strong>
                         </h5>
+                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history} />
                     </div>
                 </div>
             </div>
